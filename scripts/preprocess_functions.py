846a9a651444ca20fa1d7afd6710c3f95eafeac6
Emtinan <emtinan.s.e.osman@gmail.com>
2022-09-11 17:43:29 +0200

    modified preprocess script and notebook

--- scripts/preprocess_functions.py
+++ scripts/preprocess_functions.py
@@ -27,5 +27,35 @@ def index_by_date(df: pd.DataFrame, col_name) -> pd.DataFrame:
     dates = pd.to_datetime(df[col_name])
     df[col_name] = dates
     
-    return df.set_index(col_name)
+    return df.set_index(col_name).sort_index()
+
+
+def before_after_holidays(df: pd.DataFrame, n:int, holidayType=None, multiday = False):
+    if holidayType:
+        dates = df[df['StateHoliday'] == holidayType].index.unique().sort_values()
+    else:
+        dates = df[df['StateHoliday'] != '0'].index.unique().sort_values()
+    
+    step = 1 if multiday else 0
+    n_days = pd.to_timedelta(n, 'd')
+    a_day = pd.to_timedelta(1, 'd')
+    
+    i=0
+    while i < len(dates):
+        df.loc[dates[i]-n_days: dates[i]-a_day, 'BeforeHoliday'] = 1
+        df.loc[dates[i+step]+a_day: dates[i+step]+n_days, 'AfterHoliday'] = 1
+        i = i +1 +step
+        
+    return df
+
+
+def days_wrt_holiday(df: pd.DataFrame, public= 2, easter=5, chrismas=7):
+    df['BeforeHoliday'] = 0
+    df['AfterHoliday'] = 0
+    
+    df = before_after_holidays(df, public, holidayType = 'a')
+    df = before_after_holidays(df, easter, holidayType = 'b', multiday = True)
+    df = before_after_holidays(df, chrismas, holidayType = 'c', multiday = True)
+        
+    return df
     
